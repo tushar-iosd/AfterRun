273269ca1c789bb02ca3d2c301e5559cd2f14470
Tushar Sharma <tushar.sharma3@soprasteria.com>
2021-05-24 22:56:59 +0530

    Higher Order function start

---
+++ AfterRun/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AfterRun
+//
+//  Created by Tushar Sharma on 24/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+// Higher Order functions
+    func higherOrder(){
+        
+    }
+}
+
